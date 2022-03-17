7936e5d9db5c5ced93d9e57edb8136fe7919a6eb
Mizanur Rahman Mizan <mizanurrahmanmizan@pop-os.localdomain>
2022-03-18 05:26:49 +0600

    bolier plate and data check done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import providedData from '../../assets/data.json'
 
 export default function Home() {
+
+const data = providedData
+
+console.log(data);
+
   return (
-    <div>Home</div>
+    <div>
+      
+    </div>
   )
 }
