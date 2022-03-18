b30b33f8cf62bae46a13c320e8d13741cf51b113
Mizanur Rahman Mizan <mizanurrahmanmizan@pop-os.localdomain>
2022-03-18 15:16:39 +0600

    first 2 rows

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,66 @@
-import React from 'react'
-import providedData from '../../assets/data.json'
+import React from "react";
+import "./Home.scss";
+import providedData from "../../assets/data.json";
 
 export default function Home() {
+  const data = providedData;
 
-const data = providedData
-
-console.log(data);
+  console.log(data);
 
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="top_content">
+      <main>
+        <div className="key_section">
+          <div className="key_content">
+            <div className="text_content">
+              <h2>
+                <a href="#"> {data[6].title}</a>
+              </h2>
+              <article>{data[6].description.substring(1, 300) + "..."}</article>
+              <small> {data[6].time} ঘণ্টা আগে</small>
+            </div>
+            <figure>
+              <img src={data[6].photo} alt="key content" />
+              <small>হেফাজত নেতা মামুনুল হক | ছবি: দিপু সরকার</small>
+            </figure>
+          </div>
+          <div className="key_content_right">
+            <div className="text_content">
+              <h2>
+                <a href="#"> {data[4].title}</a>
+              </h2>
+              <article>{data[4].description.substring(1, 150) + "..."}</article>
+              <small> {data[4].time} ঘণ্টা আগে</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="second_section">
+          <div className="text_content">
+            <h2>
+              <a href="#"> {data[4].title}</a>
+            </h2>
+            <article>{data[4].description.substring(1, 150) + "..."}</article>
+            <small> {data[4].time} ঘণ্টা আগে</small>
+          </div>
+          <div className="text_content">
+            <h2>
+              <a href="#"> {data[4].title}</a>
+            </h2>
+            <article>{data[4].description.substring(1, 150) + "..."}</article>
+            <small> {data[4].time} ঘণ্টা আগে</small>
+          </div>
+          <div className="text_content right">
+            <h2>
+              <a href="#"> {data[4].title}</a>
+            </h2>
+            <article>{data[4].description.substring(1, 150) + "..."}</article>
+            <small> {data[4].time} ঘণ্টা আগে</small>
+          </div>
+        </div>
+      </main>
+
+      <aside>jlnk</aside>
+    </section>
+  );
 }
--- src/Components/partials/Header/Header.jsx
+++ src/Components/partials/Header/Header.jsx
@@ -3,11 +3,13 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <figure>
-      <img
-        src="https://assets.prothomalo.com/prothomalo/assets/palo-bangla-bb996cdb70d2e0ccec8c.svg"
-        alt="logo"
-      />
-    </figure>
+    <header>
+      <figure>
+        <img
+          src="https://assets.prothomalo.com/prothomalo/assets/palo-bangla-bb996cdb70d2e0ccec8c.svg"
+          alt="logo"
+        />
+      </figure>
+    </header>
   );
 }
